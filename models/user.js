166ab71779228873e50c007cede1c79befa9c8c9
GenioZx <eli.rodri14@email.com>
2025-07-29 12:20:28 -0400

    created vinos controller, added authUser middleware

--- models/user.js
+++ models/user.js
@@ -1,5 +1,45 @@
 const mongoose = require('mongoose');
 
+const wineSchema = mongoose.Schema ({
+  name: {
+    type: String,
+    required: true,
+  },
+  winery: {
+    type: String,
+    required: true,
+  },
+  region: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["Red", "White"],
+    required: true,
+  },
+  year: {
+    type: Date,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: [],
+    required: true
+  },
+  rating: {
+    type: Number,
+  },
+  flavorProfile: {
+    type: String,
+    enum: [],
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +49,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  wines: [wineSchema],
 });
 
 const User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -28,21 +29,25 @@ app.use(
   })
 );
 
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+// app.get('/vip-lounge', (req, res) => {
+//   if (req.session.user) {
+//     res.send(`Welcome to the party ${req.session.user.username}.`);
+//   } else {
+//     res.send('Sorry, no guests allowed.');
+//   }
+// });
 
 app.use('/auth', authController);
+const isSignedIn = require('./middleware/is-signed-in.js');
+app.use('/users/:userId/applications', applicationsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
